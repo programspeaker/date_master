2e45fdba32c5973a6903d54203bb261e555b88ef
sarathchandran <programspeaker@gmail.com>
2022-10-28 15:50:02 +0530

    version 2 uploaded - statble

--- changeformat.py
+++ changeformat.py
@@ -1,50 +1,58 @@
-from numbertoword import convert
-from validation import *
+from .numbertoword import convert
+from .validation import *
 
 
 def changeformat(date, order, format):
-    count = 0
-    for count in range(len(order)):
+    try:
+        count = 0
+        for count in range(len(order)):
 
-        if order[count] == 'DD':
-            if format[count] == 'str':
-                word = convert(date[count])
-                date[count] = word                
-            count += 1
+            if order[count] == 'DD':
+                if format[count] == 'str':
+                    word = convert(date[count])
+                    date[count] = word                
+                count += 1
 
-        elif order[count] == 'MM':
-            if format[count] == 'str':
-                word = month_constants.get(date[count])
-                date[count] = word
-            count += 1        
-        
-        elif order[count] == 'YYYY':
-            if format[count] == 'str':
-                word = convert(date[count])
-                date[count] = word
-            count += 1
-    return date
+            elif order[count] == 'MM':
+                if format[count] == 'str':
+                    word = month_constants.get(date[count])
+                    date[count] = word
+                count += 1        
+            
+            elif order[count] == 'YYYY':
+                if format[count] == 'str':
+                    word = convert(date[count])
+                    date[count] = word
+                count += 1
+        return date
+    except Exception as e:
+        message = str(e)
+        return message
 
 
 def changeformat_without_order(date, format):
-    count = 0
-    for count in range(len(date)):
+    try:
+        count = 0
+        for count in range(len(date)):
 
-        if count == 0:
-            if format[count] == 'str':
-                word = convert(date[count])
-                date[count] = word                
-            count += 1
+            if count == 0:
+                if format[count] == 'str':
+                    word = convert(date[count])
+                    date[count] = word                
+                count += 1
 
-        elif count == 1:
-            if format[count] == 'str':
-                word = month_constants.get(date[count])
-                date[count] = word
-            count += 1        
-        
-        elif count == 2:
-            if format[count] == 'str':
-                word = convert(date[count])
-                date[count] = word
-            count += 1
-    return date
+            elif count == 1:
+                if format[count] == 'str':
+                    word = month_constants.get(date[count])
+                    date[count] = word
+                count += 1        
+            
+            elif count == 2:
+                if format[count] == 'str':
+                    word = convert(date[count])
+                    date[count] = word
+                count += 1
+        return date
+    except Exception as e:
+        message = str(e)
+        return message
